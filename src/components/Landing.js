fdee6a7197f1b6e7b6a7787061762d824bbd9909
Ali Rezaei <im1.bitcode@gmail.com>
2022-10-26 15:27:32 +0330

    add spinner

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,20 +1,37 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from "react";
 
 // API
-import { getCoin } from '../services/api'
+import { getCoin } from "../services/api";
+
+// Components
+import Loader from "./Loader";
 
 const Landing = () => {
-    useEffect(() => {
-        const fetchAPI = async () => {
-            const data = await getCoin();
-            console.log(data);
-        }
-        fetchAPI()
-    }, [])
-    
-  return (
-    <div>Landing</div>
-  )
-}
+  const [coins, setCoins] = useState([]);
 
-export default Landing
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const data = await getCoin();
+      console.log(data);
+      setCoins(data);
+    };
+    fetchAPI();
+  }, []);
+
+  return (
+    <>
+      <input type="text" placeholder="Search ..." />
+      {coins.length ? (
+        <div>
+          {coins.map((coin) => (
+            <p key={coin.id}>{coin.name}</p>
+          ))}
+        </div>
+      ) : (
+        <Loader />
+      )}
+    </>
+  );
+};
+
+export default Landing;
